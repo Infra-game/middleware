4f901e586d95e0f93a4d5c95a679cf8e0b260928
idris meznad <mezdidis@hotmail.fr>
2021-11-11 14:12:02 +0100

    new

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-const express = require('express');
+/*const express = require('express');
 const app = express();
 app.listen(3000, () => {console.log('server is listening on port 3000')});
+*/
+var health = require('express-ping');
+var express = require('express');
+
+var app = express();
+
+app.use(health.ping()); // this is the only addition
+app.use(app.router);
+
+
+app.listen(3000);
