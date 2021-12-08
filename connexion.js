556b567440f9acdfd6344cd5dbaaf09bac51e482
John-Kenneth Taylor Afonah <johnkta7@gmail.com>
2021-12-08 14:53:27 +0100

    Fix users

--- connexion.js
+++ connexion.js
@@ -6,34 +6,35 @@ const saltRounds = 10;
 module.exports = (app, db) => {
     app.post("/login", (req, res) => {
         db.getConnection((err, connection) => {
-            if(err) throw err;
-
-            const params = {
-                email : req.body.email,
-                username : req.body.username,
-                password : req.body.password,
-            };
-
-            connection.query('SELECT * FROM users WHERE email = ? AND username = ?', [params.email, params.username], (err, result) => {
-                connection.release();
-
-                if(!err) {
-                    if(result.length > 0) {
-                        bcrypt.compare(params.password, result[0].password, (err, response) => {
-                            if(response) {
-                                const accessToken = jwt.sign({id: result[0].id, username: result[0].username, role: result[0].role}, process.env.BCRYPT_SECRET);
-                                res.json({error : false, message : "Connexion réussie.", accessToken});
-                            } else {
-                                res.json({error: true, message : "Mot de passe incorrect."})
-                            }
-                        })
+            if(err) res.json(err);
+            else {
+                const params = {
+                    email : req.body.email,
+                    username : req.body.username,
+                    password : req.body.password,
+                };
+    
+                connection.query('SELECT * FROM users WHERE email = ? AND username = ?', [params.email, params.username], (err, result) => {
+                    connection.release();
+    
+                    if(!err) {
+                        if(result.length > 0) {
+                            bcrypt.compare(params.password, result[0].password, (err, response) => {
+                                if(response) {
+                                    const accessToken = jwt.sign({id: result[0].id, username: result[0].username, role: result[0].role}, process.env.BCRYPT_SECRET);
+                                    res.json({error : false, message : "Connexion réussie.", accessToken});
+                                } else {
+                                    res.json({error: true, message : "Mot de passe incorrect."})
+                                }
+                            })
+                        } else {
+                            res.json({error: true, message: "Cet utilisateur n'existe pas."})
+                        }
                     } else {
-                        res.json({error: true, message: "Cet utilisateur n'existe pas."})
+                        console.log(err);
                     }
-                } else {
-                    console.log(err);
-                }
-            })
+                })
+            }
         })
     })
 
@@ -45,7 +46,7 @@ module.exports = (app, db) => {
                 email : req.body.email,
                 username : req.body.username,
                 password : req.body.password,
-                fullName : req.body.fullName,
+                fullName : "c moi wsh",
                 role : req.body.role
             };
 
